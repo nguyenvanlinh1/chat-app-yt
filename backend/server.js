4f0df0535a71758c9e24845559866f3f662c815e
nguyenvanlinh1 <nvanlinh1406@gmail.com>
2024-03-04 00:02:38 +0700

    finally

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -12,6 +13,8 @@ import { app, server } from "./socket/socket.js";
 
 const POST = process.env.POST || 5000
 
+const __dirname = path.resolve();
+
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
@@ -21,6 +24,11 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 // app.get("/", (req, res) => {
 //     res.send("Hello World")
 // })
